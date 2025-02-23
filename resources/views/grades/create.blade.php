1158113cb8d1b99afe5678ca218f558f604ffa23
Spade-kun <2201104232@student.buksu.edu.ph>
2025-02-23 15:25:31 +0800

    Final debug

--- resources/views/grades/create.blade.php
+++ resources/views/grades/create.blade.php
@@ -31,7 +31,7 @@
             <div class="card shadow mb-4">
                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                     <h6 class="m-0 font-weight-bold text-primary">
-                        <i class="fas fa-graduation-cap mr-2"></i>Grade Information
+                        <i class="fas fa-graduation-cap mr-2"></i>Grade Form
                     </h6>
                 </div>
                 <div class="card-body">
